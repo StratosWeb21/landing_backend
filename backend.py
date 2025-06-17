5d0406a96812534ce6778c694e226bc163ff8371
StratosWeb21 <papposstratos@gmail.com>
2025-06-17 20:33:08 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -37,4 +37,5 @@ def submit():
     return jsonify({"success": True, "message": "Εγγραφή επιτυχής."})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
